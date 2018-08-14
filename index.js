1f557f6dfe37eef6a50bdb39b2cecbfd871adf5f
Sergey Lebedev <oddyone@mail.ru>
2018-08-15 00:52:42 +0300

    Added first component

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-/** @format */
+import React from 'react';
+import {Text, AppRegistry} from 'react-native';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// Create a component
+const App = () => {
+    return (
+        <Text>Some Text</Text>
+    )
+};
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('albums2', () => App);
