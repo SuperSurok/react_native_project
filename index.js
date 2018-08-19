74cc49dfbe201d593cac4d9dc42a573d3ed876f9
Sergey Lebedev <oddyone@mail.ru>
2018-08-19 22:21:22 +0300

    Added Header component.

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 // Import a library to help create a component
 import React from 'react';
-import {AppRegistry} from 'react-native';
-import Header from './src/components/Header';
+import {View, AppRegistry} from 'react-native';
 
+// Components
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 // Create a component
 const App = () => {
     return (
-        <Header/>
+        <View>
+            <Header
+                headerText={'Albums'}
+            />
+            <AlbumList/>
+        </View>
     )
 };
 // Render it to the device
