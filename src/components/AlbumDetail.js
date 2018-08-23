875cb087fe77c12e179783e15a793a6e6de03c9d
Sergey Lebedev <oddyone@mail.ru>
2018-08-23 22:23:04 +0300

    Added header content

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,15 +1,38 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View, Image} from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection'
-const AlbumDetail = (props) => {
+
+const AlbumDetail = ({album}) => {
+    const {title, artist, thumbnail_image} = album;
+    const {thumbnailStyle, headerContentSytle} = styles;
     return (
         <Card>
             <CardSection>
-                <Text>{props.album.title}</Text>
+                <View>
+                    <Image
+                        style={thumbnailStyle}
+                        source={{uri: thumbnail_image}}
+                    />
+                </View>
+                <View style={headerContentSytle}>
+                    <Text>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
             </CardSection>
         </Card>
     );
+};
+
+const styles = {
+    headerContentSytle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    }
 };
 
 export default AlbumDetail;
